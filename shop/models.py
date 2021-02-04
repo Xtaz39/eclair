4bc3b9f8285a0bb3220db1463955fcc6d5e436bc
Denis Fetinin <denka0087@gmail.com>
2021-02-04 09:19:32 +0300

    add order

--- shop/models.py
+++ shop/models.py
@@ -37,3 +37,24 @@ class Cart(models.Model):
     product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
     amount = models.IntegerField("Количество продукта", default=1)
     customer = models.ForeignKey(to=Customer, on_delete=models.CASCADE)
+
+
+class Order(models.Model):
+    """Заказ"""
+
+    customer_name = models.CharField("Имя заказчика", max_length=100)
+    phone = PhoneField("Номер телефона")
+    address = models.TextField("Адрес", max_length=150)
+    payment_type = models.CharField(
+        "Способ оплаты",
+        choices=(("cash", "Наличными"), ("card", "Картой на сайте")),
+        max_length=100,
+    )
+
+
+class OrderProduct(models.Model):
+    title = models.CharField("Название", max_length=100)
+    category = models.CharField("Категория", max_length=100)
+    price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
+    amount = models.IntegerField("Количество продукта", default=1)
+    order = models.ForeignKey(Order, on_delete=models.DO_NOTHING)
