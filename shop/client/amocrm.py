c7764d2d7c79a932ed121b138fe33afdf67752d7
Denis Fetinin <denka0087@gmail.com>
2021-02-21 13:59:56 +0300

    Fix annotations on python 3.7

--- shop/client/amocrm.py
+++ shop/client/amocrm.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 import enum
 import logging
 from dataclasses import dataclass
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from django import forms
 from django.core.handlers.wsgi import WSGIRequest
 from django.db import transaction
