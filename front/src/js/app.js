95501fcae285ea7a4da2a75b01e1b3a807993337
Dmytro Puzyrenko <myaczt@gmail.com>
2021-03-13 14:12:26 +0200

    update cake constructor layout, refactor code, add eslilt

--- front/src/js/app.js
+++ front/src/js/app.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
-import 'slick-carousel';
 import 'ion-rangeslider';
+import './sliders';
+import './cake-constructor';
 
 if (module.hot) {
   module.hot.accept();
@@ -41,54 +42,6 @@ $(window).on('load', () => {
     $('.header__wrap').toggleClass('active');
   });
 
-  $('.hero__slider').slick({
-    dots: false,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    pauseOnFocus: true,
-    pauseOnHover: true,
-    prevArrow: $('.slider__buttons--prev'),
-    nextArrow: $('.slider__buttons--next'),
-  });
-
-  $('.carousel').slick({
-    infinite: true,
-    prevArrow: $('.buttons .prev'),
-    nextArrow: $('.buttons .next'),
-  });
-
-  $('.carousel-recommended').slick({
-    infinite: true,
-    slidesToShow: 2,
-    arrows: false,
-    responsive: [
-      {
-        breakpoint: 500,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  });
-
-  $('.items-switch__top').slick({
-    infinite: true,
-    arrows: false,
-    slidesToShow: 1,
-    asNavFor: $('.items-switch__bottom'),
-  });
-
-  $('.items-switch__bottom').slick({
-    infinite: true,
-    arrows: false,
-    slidesToShow: 6,
-    focusOnSelect: true,
-    asNavFor: $('.items-switch__top'),
-  });
-
   $('.item__actions').on('click', function actionItemsClickHandler(e) {
     const num = +$(this).find('.item__quantity-text').text();
 
@@ -106,133 +59,32 @@ $(window).on('load', () => {
     }
   });
 
-  $('.add-address').on('click', function addAddressHandler(e) {
+  $('.add-address').on('click', (e) => {
     e.preventDefault();
     $('.address-field').first().clone().appendTo('.fields');
   });
 
-
-  // CAKE CONSTRUCTOR
-  (function() {
-    var e = $(".js-rangeInput")
-      , t = parseFloat($(".js-rangeInput").attr("data-min-weight"))
-      , n = parseFloat($(".js-rangeInput").attr("data-max-weight"));
-    e.ionRangeSlider({
-      min: t,
-      max: n,
-      step: .5,
-      postfix: " кг",
-      grid: !1,
-      onChange (data) {
-        $('.cake-weight').text(data.from)
-        $('input#weight').val(data.from)
-        const peopleCount = (data.from * 6.5).toFixed(0)
-        $('.range-wrap__descr .peoples').text(peopleCount)
-      }
-    })
-  })();
-
-  $('a[data-feature-select]').on('click', function (e) {
-    e.preventDefault()
-    const action = $(this).data('feature-select');
-
-    switch (action) {
-      case 'filling':
-        $('.feature-modal.feature-filling').addClass('active')
-        break
-      case 'design':
-        $('.feature-modal.feature-design').addClass('active')
-        break
-    }
-  })
-  $('.get-back-btn-link').on('click', function (e) {
-    e.preventDefault()
-    $('.feature-modal').removeClass('active')
-  })
-
-  const filledIds = new Set()
-  const cakeData = {
-    fillIds: [],
-    designId: ''
-  }
-
-  $('.feature-filling__item-btn[data-feature-fill]').on('click', function (e) {
-    e.preventDefault()
-    const parent = $(this).parent('.feature-filling__item')
-    const fill = {
-      title: parent.find('.feature-filling__item-title').text(),
-      src: parent.find('img').attr('src'),
-      data: parent.data('feature-fill')
-    }
-    if (filledIds.has(fill.data)) { return }
-
-    const image = `
-    <div class="cake-filling-item" data-feature-fill="${fill.data}">
-      <img class="cake-filling-item__image" src="${fill.src}">
-      <button class="cake-filling-item__remove" data-feature-fill="${fill.data}"></button>
-      <span class="cake-filling-item__name">${fill.title}</span>
-    </div>
-    `
-    $('.cake-order-images').append(image)
-    parent.addClass('selected')
-
-    filledIds.add(fill.data)
-    cakeData.fillIds.push(fill.data)
-    $('input#fills').val(cakeData.fillIds.join(','))
-
-    // refresh click handler
-    $('.cake-filling-item__remove').on('click', function () {
-      const fillData = $(this).data('feature-fill')
-      filledIds.delete(fillData)
-      cakeData.fillIds = cakeData.fillIds.filter(id => id !== fillData)
-      $('input#fills').val(cakeData.fillIds.join(','))
-      $(`.feature-filling__item[data-feature-fill=${fillData}]`).removeClass('selected')
-      $(`.cake-filling-item[data-feature-fill=${fillData}]`).remove()
-    })
-  })
-
-  $('.feature-filling__item-btn[data-feature-design]').on('click', function (e) {
-    e.preventDefault()
-    $('.feature-design .feature-filling__item').removeClass('selected')
-    const parent = $(this).parent('.feature-filling__item')
-    const fill = {
-      src: parent.find('img').attr('src'),
-      data: parent.data('feature-design')
-    }
-
-    const image = `<div class="image" style="background-image: url(${fill.src})"></div>`
-
-    $('.cake-design-images').empty()
-    $('.cake-design-images').append(image)
-    parent.addClass('selected')
-
-    cakeData.designId = fill.data
-    $('input#design').val(cakeData.designId)
-  })
-  // CAKE CONSTRUCTOR END
-
   $('.modal__close').on('click', function (e) {
-    e.preventDefault()
-    $(this).parent().parent('.modal').removeClass('active')
-  })
-  $('.login-btn').on('click', function (e) {
-    e.preventDefault()
-    const stage1Input = $('.login-stage-1')
-    const stage2Input = $('.login-stage-2')
-    if(stage2Input.hasClass('active')) {
+    e.preventDefault();
+    $(this).parent().parent('.modal').removeClass('active');
+  });
+  $('.login-btn').on('click', (e) => {
+    e.preventDefault();
+    const stage1Input = $('.login-stage-1');
+    const stage2Input = $('.login-stage-2');
+    if (stage2Input.hasClass('active')) {
       // submit form
     } else {
-      stage1Input.removeClass('active')
-      stage2Input.addClass('active')
-      stage1Input.find('input').prop('disabled', true)
-      stage1Input.find('button').show()
+      stage1Input.removeClass('active');
+      stage2Input.addClass('active');
+      stage1Input.find('input').prop('disabled', true);
+      stage1Input.find('button').show();
     }
-  })
+  });
 
   $('.input button').on('click', function () {
-    $('.login-stage-2').removeClass('active')
-    $(this).siblings('input').prop('disabled', false)
-    $(this).hide()
-  })
-
+    $('.login-stage-2').removeClass('active');
+    $(this).siblings('input').prop('disabled', false);
+    $(this).hide();
+  });
 });
---
+++ front/src/js/cake-constructor/index.js
@@ -0,0 +1,117 @@
+// CAKE CONSTRUCTOR
+import $ from 'jquery';
+
+(function () {
+  const range = $('.js-rangeInput');
+  const min = parseFloat($('.js-rangeInput').attr('data-min-weight'));
+  const max = parseFloat($('.js-rangeInput').attr('data-max-weight'));
+  range.ionRangeSlider({
+    min,
+    max,
+    step: 0.5,
+    postfix: ' кг',
+    grid: !1,
+    onChange(data) {
+      $('.cake-weight').text(data.from);
+      $('input#weight').val(data.from);
+      const peopleCount = (data.from * 6.5).toFixed(0);
+      $('.range-wrap__descr .peoples').text(peopleCount);
+    },
+  });
+}());
+
+$('a[data-feature-select]').on('click', function (e) {
+  e.preventDefault();
+  const action = $(this).data('feature-select');
+
+  switch (action) {
+    case 'filling':
+      $('.feature-modal.feature-filling').addClass('active');
+      break;
+    case 'design':
+      $('.feature-modal.feature-design').addClass('active');
+      break;
+    case 'decor':
+      $('.feature-modal.feature-decor').addClass('active');
+      break;
+    case 'postcard':
+      $('.feature-modal.feature-postcard').addClass('active');
+      break;
+    default:
+      return 0;
+  }
+  return 0;
+});
+$('.get-back-btn-link').on('click', (e) => {
+  e.preventDefault();
+  $('.feature-modal').removeClass('active');
+});
+
+const cakeData = {
+  featuresList: {},
+  designId: '',
+};
+// Multiple choose of feature
+function initFeatures(features) {
+  features.forEach((feature) => {
+    cakeData.featuresList[feature] = new Set();
+    $(`.feature-filling__item-btn[data-${feature}]`).on('click', function (e) {
+      e.preventDefault();
+      const parent = $(this).parent('.feature-filling__item');
+      const fill = {
+        title: parent.find('.feature-filling__item-title').text(),
+        src: parent.find('img').attr('src'),
+        data: parent.data(feature),
+      };
+      if (cakeData.featuresList[feature].has(fill.data)) { return; }
+
+      const image = `
+        <div class="cake-filling-item" data-${feature}="${fill.data}">
+          <img class="cake-filling-item__image" src="${fill.src}">
+          <button class="cake-filling-item__remove" data-${feature}="${fill.data}"></button>
+          <span class="cake-filling-item__name">${fill.title}</span>
+        </div>
+        `;
+      $(`.${feature}-images`).append(image);
+      parent.addClass('selected');
+
+      cakeData.featuresList[feature].add(fill.data);
+      $(`input#${feature}`).val(Array.from(cakeData.featuresList[feature]).join(','));
+
+      // refresh click handler
+      $('.cake-filling-item__remove').on('click', function () {
+        const fillData = $(this).data(feature);
+        cakeData.featuresList[feature].delete(fillData);
+        $(`input#${feature}`).val(Array.from(cakeData.featuresList[feature]).join(','));
+        $(`.feature-filling__item[data-${feature}=${fillData}]`).removeClass('selected');
+        $(`.cake-filling-item[data-${feature}=${fillData}]`).remove();
+      });
+    });
+  });
+}
+initFeatures([
+  'feature-fill',
+  'feature-decor',
+  'feature-postcard',
+]);
+// Single choose of feature
+// @Todo: refactor it for dynamic feature
+$('.feature-filling__item-btn[data-feature-design]').on('click', function (e) {
+  e.preventDefault();
+  $('.feature-design .feature-filling__item').removeClass('selected');
+  const parent = $(this).parent('.feature-filling__item');
+  const fill = {
+    src: parent.find('img').attr('src'),
+    data: parent.data('feature-design'),
+  };
+
+  const image = `<div class="image" style="background-image: url(${fill.src})"></div>`;
+
+  $('.cake-design-images').empty();
+  $('.cake-design-images').append(image);
+  parent.addClass('selected');
+
+  cakeData.designId = fill.data;
+  $('input#design').val(cakeData.designId);
+});
+// CAKE CONSTRUCTOR END
