2f92d8efedf019fc18b2702af7c863be5d3d722e
Denis Fetinin <denka0087@gmail.com>
2021-03-14 19:42:44 +0300

    fix path

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -3,8 +3,8 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 
-sys.path.insert(0, BASE_DIR)
-sys.path.insert(1, BASE_DIR / ".venv/lib/python3.9/site-packages")
+sys.path.insert(0, str(BASE_DIR))
+sys.path.insert(1, str(BASE_DIR / ".venv/lib/python3.9/site-packages"))
 
 from eclair import wsgi
 
